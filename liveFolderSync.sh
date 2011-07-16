0f3323dbaf69fa36b7cd26a09ef8780f2df1edb3
Simon Green <simonjgreen@me.com>
2011-07-16 10:34:07 +0100

    Adding logger

--- liveFolderSync.sh
+++ liveFolderSync.sh
@@ -11,12 +11,22 @@ BASEDIR="$1"
 TARGETSERVER="$2"
 RSYNC="rsync -av --delete -e ssh"
 
+#Logger Function
+log() { logger -p daemon.info -t "liveFolderSync" "$@"; echo "$@"; }
+
+#Make sure any running sync completes before exiting
+finishSyncAndExiti() { log "Completing currently running syncs";   }
+trap finishSyncAndExit SIGINT
+
 # Initial sync
+log "Performing initial sync"
 $RSYNC ${BASEDIR}/* ${TAGRETSERVER}:${BASEDIR}/
+log "Initial sync complete"
 
 # Wait for individual file events and keep in sync
 inotifywait --format '%e %w' -e close_write -e move -e create -e delete -qmr $BASEDIR | while read EVENT DIR
 do
 	# Fork off rsync proc to do sync
 	$RSYNC ${DIR}/* ${TAGRETSERVER}:${DIR}/ &
+	log "${EVENT} on ${DIR} triggered sync with PID $!"
 done
